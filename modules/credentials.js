57a16f6041619e02a99ab8985fcb8a8b8f4cc1ca
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2022-03-03 12:00:28 -0300

    Arreglado error .gitignore

--- modules/credentials.js
+++
@@ -1,8 +0,0 @@
-exports.sessionSecret = "adfsadsafdsafjhbsdfahbjfjhfhewqfwebjhfqjhwqefjhefwqjwefqjefwqwqf";
-exports.mysql = {
-    ip: "db-lorenzord-ft.c6q37hmz1t5w.sa-east-1.rds.amazonaws.com",
-    username: "node.server",
-    password: "a95YCmwkS8WrcZU8",
-    database: "DB_2021_5BINF_G03",
-    port: 3306
-}
