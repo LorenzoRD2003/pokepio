59ffc93686c6f0c2643d4d0f3f73cbb9badc202f
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2022-03-03 11:58:14 -0300

    Probando

--- modules/credentials.js
+++ modules/credentials.js
@@ -1,8 +1,8 @@
 exports.sessionSecret = "adfsadsafdsafjhbsdfahbjfjhfhewqfwebjhfqjhwqefjhefwqjwefqjefwqwqf";
 exports.mysql = {
-    ip: "db-proyectos-lorenzord.c6q37hmz1t5w.sa-east-1.rds.amazonaws.com",
+    ip: "db-lorenzord-ft.c6q37hmz1t5w.sa-east-1.rds.amazonaws.com",
     username: "node.server",
-    password: "SvDWW5DZBmxmu47eRDJMSt4p",
+    password: "a95YCmwkS8WrcZU8",
     database: "DB_2021_5BINF_G03",
     port: 3306
 }
